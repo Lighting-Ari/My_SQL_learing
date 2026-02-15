f357abda16f2ec536290070ea5a245aabab0a019
Lighting-Ari <roydavil@gmail.com>
2026-02-15 21:28:05 +0530

    last update before 15 feb 2026

--- Learning.sql
+++ Learning.sql
@@ -976,9 +976,23 @@ WHERE o.customer_id = '17'
 ORDER BY o.product_id;
 
 
+-- Right Join
 
+SHOW DATABASES;
+USE coffee_store;
+SHOW TABLES;
 
+SELECT o.*, c.* FROM customers c
+LEFT JOIN orders o ON o.customer_id = c.id
+ORDER BY o.id;
 
+SELECT o.*, c.* FROM customers c
+RIGHT JOIN orders o ON o.customer_id = c.id
+ORDER BY o.id;
+
+SELECT o.*, c.* FROM orders o
+RIGHT JOIN customers c ON o.customer_id = c.id
+ORDER BY o.order_time;
 
 
 
